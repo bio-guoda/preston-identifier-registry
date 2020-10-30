a67f988ef0b38445c587be6440b69893c07d4470
jhpoelen <jhpoelen@xs4all.nl>
2020-10-30 15:02:10 -0700

    embed gzip in script

--- build-link-registry.sh
+++ build-link-registry.sh
@@ -2,5 +2,4 @@
 
 PATTERN_NAME=${1:-email}
 
-./build-datasets-containing-pattern.sh ${PATTERN_NAME} | sort | uniq | ./build-datasets-matched-values.sh ${PATTERN_NAME} | sort | uniq
-
+./build-datasets-containing-pattern.sh ${PATTERN_NAME} | sort | uniq | ./build-datasets-matched-values.sh ${PATTERN_NAME} | sort | uniq | gzip
