c55bd7783df5ab6d4f5c0f265a7f5754059aded6
jhpoelen <jhpoelen@xs4all.nl>
2020-11-05 16:13:08 -0800

    make producer role explicit

--- populate-link-registry.sh
+++ populate-link-registry.sh
@@ -13,5 +13,5 @@
 
 PATTERN_NAME=${1:-email}
 date
-time ./build-datasets-matched-values.sh ${PATTERN_NAME} | kafkacat -b localhost:9092 -t ${PATTERN_NAME} -K '|'
+time ./build-datasets-matched-values.sh ${PATTERN_NAME} | kafkacat -P -b localhost:9092 -t ${PATTERN_NAME} -K '|'
 date
