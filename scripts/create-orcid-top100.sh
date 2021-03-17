32a89d7565760d44972c9059d7e4cae023465633
jhpoelen <jhpoelen@xs4all.nl>
2021-03-17 13:49:33 -0700

    add single line script to calculate top 100 of most frequently cited ORCIDs in GBIF/iDigBio networks fyi @qgroom : looks like you are on position 19!

---
+++ scripts/create-orcid-top100.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Creates top 100 of most frequently cited in biodiversity data
+#
+# Note: depending on your internet bandwidth, it might take a while to download, and scan through, ~1TB of data. 
+#
+
+preston ls --no-cache --remote https://deeplinker.bio,https://archive.org/download/biodiversity-dataset-archives/data.zip/data/\
+| preston -l tsv --no-cache grep "0000-00[0-9]{2}-[0-9]{4}-[0-9]{4}"\
+| grep "http://www.w3.org/ns/prov#value"\
+| cut -f3\
+| sort\
+| uniq -c\
+| sort -nr\
+| head 100
+
