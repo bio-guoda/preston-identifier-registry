766b2c2c692d69d7fa0705ef242689e6c413e80b
jhpoelen <jhpoelen@xs4all.nl>
2021-03-17 07:21:12 -0700

    add 3-hash description

--- scripts/count-identifier-scheme-usage.sh
+++ scripts/count-identifier-scheme-usage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Count the number of times a identifier scheme is used in a 3-hash . 
+# Count the number of times a identifier scheme is used in a 3-hash . A 3-hash bin groups all content with same first three sha256 characters. 
 #
 # usage:
 #  ./count-identifier-scheme-usage.sh [identifier scheme]
@@ -9,7 +9,5 @@
 #  ./count-identifier-scheme-usage ark
 #
 
-
-
-./consume-link-registry.sh meise | pv -l | grep -o -E "hash://sha256/[a-f0-9]+" | sed 's/sha256//g' | sort | uniq | grep -o -E "[a-f0-9]{3}" | sort | uniq -c | sort -nr | sed -E 's/^[ ]+//g' | awk -F ' ' '{ print $2 "\t" $1 }' | sort | tee meise-freq.tsv
+./consume-link-registry.sh meise | pv -l | grep -o -E "hash://sha256/[a-f0-9]+" | sed 's/sha256//g' | sort | uniq | grep -o -E "[a-f0-9]{3}" | sort | uniq -c | sort -nr | sed -E 's/^[ ]+//g' | awk -F ' ' '{ print $2 "\t" $1 }' | sort
 
