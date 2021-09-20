8e050c978dc5cf1c6da37c4124fa0e4cd1b9c2e1
jhpoelen <jhpoelen@xs4all.nl>
2021-09-20 09:53:12 -0700

    simplify resolve-orchid script

--- scripts/resolve-orcid.sh
+++ scripts/resolve-orcid.sh
@@ -7,15 +7,9 @@
 #  Kojun Kanda
 #
 
-resolve_orcids() {
   # resolve names associated with the orcids
-  sed -E 's/^[ ]+//g'\
-  | tr ' ' '\t'\
-  | cut -f2\
-  | awk '{ print "https://orcid.org/" $1 "/person.json" }'\
-  | xargs -L1 curl --silent\
-  | jq --raw-output .displayName
-}
+awk '{ print "https://orcid.org/" $1 "/person.json" }'\
+| xargs -L1 curl --silent\
+| jq --raw-output .displayName
 
-resolve_orcids
 
